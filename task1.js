a7cf532000c3a67619b33a1a36b495b836ac9c25
Ayomide Ojelade <ojeladea@outlook.com>
2021-11-25 13:27:43 +0000

    task 1

--- task1.js
+++ task1.js
@@ -1,3 +1,13 @@
+// function that takes in a string and turns it uppercase
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats =  cats.map(cat => cat.toUpperCase())
+console.log(capitalisedCats);
+
+
+/*function turnUpperCase() {
+
+}*/
+
+
+
