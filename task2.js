0b6fd961b3c423350d9f92def9345de3d0515662
Ayomide Ojelade <ojeladea@outlook.com>
2021-11-25 14:47:49 +0000

    task 2

--- task2.js
+++ task2.js
@@ -8,4 +8,12 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+  /*let result = data.filter(f => f.type.toLowerCase().startsWith('p'))
+console.log(result)*/
+
+const bListAnimals = animals.filter(animal => animal.startsWith("b"));
+ 
+console.log(bListAnimals)
+
+// Your 'filter' code here.
+
